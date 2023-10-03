9fd92a85d595e3246fb26b9117e455cf455eb6e1
Orsiris <DANIELWONG.22@ichat.sp.edu.sg>
2023-10-04 01:52:31 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+import emailjs from '@emailjs/browser';
+
+const emailApiKey = import.meta.env.VITE_EMAIL_KEY;
+const serviceId = import.meta.env.VITE_SERVICE_ID;
+const templateId = import.meta.env.VITE_TEMPLATE_ID;
+
+const scrollButton = document.querySelector('.landingButton');
+
+scrollButton.addEventListener('click', function (event) {
+    event.preventDefault(); // Prevent the default anchor link behavior
+
+    // Get the target section (in this case, the "works" section)
+    const targetSection = document.getElementById('works')
+
+    // Scroll to the target section with smooth behavior
+    targetSection.scrollIntoView({ behavior: 'smooth' });
+  });
+
+emailjs.init(emailApiKey);
+
+window.onload = function() {
+
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        emailjs.sendForm(serviceId, templateId, this)
+            .then(function() {
+                console.log('SUCCESS!');
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+    });
+}
